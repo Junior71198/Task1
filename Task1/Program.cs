eed94c26995c546df8f24ac3b62d83b30001b2c6
Junior71198 <mohamedkamel11988@gmail.com>
2026-02-06 20:57:53 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.WriteLine("Number of small carpets:");
+            int sc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of large carpets:");
+            int lc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price per small carpets: $25");
+            Console.WriteLine("Price per large carpets: $35");
+            int cost = (sc * 25) + (lc * 35);
+            Console.WriteLine($"cost: {cost}");
+            double tx = (cost * 0.06);
+            Console.WriteLine($"tax: {tx}");
+            double total = cost + tx;
+            Console.WriteLine($"Total estimate:{total}");
+        }
+    }
+}
